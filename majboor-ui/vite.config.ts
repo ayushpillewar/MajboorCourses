544c7607f7ed5c52b4324c4f792a7d68e59c94a4
aayus <aayushpllwr@gmail.com>
2025-09-08 23:04:45 +0530

    more server config

--- majboor-ui/vite.config.ts
+++ majboor-ui/vite.config.ts
@@ -7,5 +7,13 @@ export default defineConfig({
   server: {
     port: 8080,
     host: true // Listen on all addresses
-  }
+  },
+   build: {
+    // Specify the output directory for the production build
+    // This is where your compiled assets will be placed for serving
+    outDir: 'dist', 
+    
+    // Set to true if you want to generate sourcemaps for debugging production builds
+    sourcemap: false, 
+  },
 })
