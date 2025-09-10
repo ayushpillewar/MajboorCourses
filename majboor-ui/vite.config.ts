4e83fb232013a96754ce0cf415c8aa8ef29be54c
aayus <aayushpllwr@gmail.com>
2025-09-11 00:47:31 +0530

    ssl

--- majboor-ui/vite.config.ts
+++ majboor-ui/vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import fs from 'fs'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+          key: fs.readFileSync('private.pem'), // Path to your SSL private key
+          cert: fs.readFileSync('certificate.pem'), // Path to your SSL certificate
+    },
     port: 443,
     host: true // Listen on all addresses
   },
